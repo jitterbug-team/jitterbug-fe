6754f708c1fcc4a509b037439b10eb47bc91f557
davide.patti <davide.patti@autotrader.co.uk>
2019-10-27 08:39:53 +0000

    handle the accept/decline task buttons

--- app/screens/TaskDetails.js
+++ app/screens/TaskDetails.js
@@ -2,6 +2,7 @@ import React from 'react';
 import axios from "axios";
 import { Button } from 'react-native-elements';
 import { View, Dimensions, StyleSheet, ScrollView, Text, Image } from 'react-native';
+import Toast from 'react-native-root-toast';
 
 const SCREEN_WIDTH = Dimensions.get('window').width;
 const SCREEN_HEIGHT = Dimensions.get('window').height;
@@ -19,7 +20,8 @@ class TaskDetails extends React.Component {
     };
 
     state = {
-        task: []
+        task: [],
+        acceptButton : false
     }
 
     componentDidMount() {
@@ -40,9 +42,79 @@ class TaskDetails extends React.Component {
             });
     }
 
+    getHelperId = () => {
+        const helperId = '5db54f7ad6190c4e3f74ec04'
+        return helperId
+    }
+
+    acceptTaskButton = (taskId) => {
+        const helperId = this.getHelperId()
+        console.log('taskId', taskId)
+        axios.put(`https://jitterbug-service.herokuapp.com/task/accept-task`,
+            { helperId, taskId })
+            .then((response) =>{
+                console.log(response.status)
+                console.log(response.data)
+                if(response.status === 200)
+                    this.setState({
+                        acceptButton: true
+                })
+            }).then(()=>{
+            this.state.acceptButton ?
+                Toast.show('Task accepted', {
+                    duration: Toast.durations.LONG,
+                    position: Toast.positions.CENTER,
+                    shadow: true,
+                    animation: true,
+                    hideOnPress: true,
+                    delay: 0.3
+            }) : Toast.show('Sorry, something went wrong :(', {
+                    duration: Toast.durations.LONG,
+                    position: Toast.positions.CENTER,
+                    shadow: true,
+                    animation: true,
+                    hideOnPress: true,
+                    delay: 0.3
+                })
+        })
+    }
+
+    declineTaskButton = (taskId) => {
+        const helperId = this.getHelperId()
+        console.log('taskId', taskId)
+        axios.put(`https://jitterbug-service.herokuapp.com/task/cancel-task`,
+            { helperId, taskId })
+            .then((response) =>{
+                console.log(response.status)
+                console.log(response.data)
+                if(response.status === 200)
+                    this.setState({
+                        acceptButton: false
+                    })
+            }).then(()=>{
+            !this.state.acceptButton ?
+                Toast.show('Task declined', {
+                    duration: Toast.durations.LONG,
+                    position: Toast.positions.CENTER,
+                    shadow: true,
+                    animation: true,
+                    hideOnPress: true,
+                    delay: 0.3
+                }) : Toast.show('Sorry, something went wrong :(', {
+                    duration: Toast.durations.LONG,
+                    position: Toast.positions.CENTER,
+                    shadow: true,
+                    animation: true,
+                    hideOnPress: true,
+                    delay: 0.3
+                })
+        })
+
+    }
+
     render (){
-        console.log(this.state.tasks)
-        const {name, description, categoryImage, personInNeed} = this.state.task
+        //console.log(this.state.task)
+        const {id, name, description, categoryImage, personInNeed} = this.state.task
         const {firstName, image} = personInNeed ? personInNeed : ''
         return (
             <ScrollView style={styles.container} >
@@ -140,19 +212,28 @@ class TaskDetails extends React.Component {
                         </Text>
                     </View>
                     <View style={[styles.buttonsContainer, { marginBottom: 20 }]}>
-                        <Button
-                            title="Accept"
-                            buttonStyle={{ backgroundColor: 'rgba(127, 220, 103, 1)' }}
-                            containerStyle={{ height: 40 }}
-                            titleStyle={{ color: 'white', marginHorizontal: 20 }}
-                        />
-                        <Button
-                            title="Decline"
-                            buttonStyle={{ backgroundColor: 'rgba(214, 61, 57, 1)' }}
-                            containerStyle={{ height: 40 }}
-                            type="outline"
-                            titleStyle={{ color: 'white', marginHorizontal: 20 }}
-                        />
+                        {
+                            !this.state.acceptButton ?
+                                <Button
+                                    title="Accept"
+                                    buttonStyle={{ backgroundColor: 'rgba(127, 220, 103, 1)' }}
+                                    containerStyle={{ height: 40 }}
+                                    titleStyle={{ color: 'white', marginHorizontal: 20 }}
+                                    onPress={()=> this.acceptTaskButton(id)}
+                                />
+                                :
+                                <Button
+                                    title="Decline"
+                                    buttonStyle={{ backgroundColor: 'rgba(214, 61, 57, 1)' }}
+                                    containerStyle={{ height: 40 }}
+                                    type="outline"
+                                    titleStyle={{ color: 'white', marginHorizontal: 20 }}
+                                    onPress={()=> this.declineTaskButton(id)}
+                                />
+                        }
+
+
+
                     </View>
                 </View>
             </ScrollView>
